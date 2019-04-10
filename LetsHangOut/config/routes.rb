5d87c5a50dfa4e88ce9354e4c1afb53fe286c6e9
Edison Zapata <zed15600@gmail.com>
2019-04-10 18:19:36 -0500

    Creando el formulario para agregar un nuevo restaurante

--- LetsHangOut/config/routes.rb
+++ LetsHangOut/config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'restaurantes/index'
-  get 'restaurantes/show'
-  get 'restaurantes/new'
-  get 'restaurantes/edit'
-  get 'restaurantes/create'
-  get 'restaurantes/update'
-  get 'restaurantes/destroy'
   get 'welcome/index'
-  
+
   resources :personas
+  resources :restaurantes
 
   root "welcome#index"
 end
