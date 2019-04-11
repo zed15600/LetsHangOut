c7cfce88c3ecb390ac9d149739726fd3e52ee9ad
Edison Zapata <zed15600@gmail.com>
2019-04-11 13:39:08 -0500

    configuring gitignore

--- LetsHangOut/app/controllers/restaurantes_controller.rb
+++ LetsHangOut/app/controllers/restaurantes_controller.rb
@@ -1,17 +1,26 @@
 class RestaurantesController < ApplicationController
+
   def index
+    @restaurantes = Restaurante.all
   end
 
   def show
   end
 
   def new
+    @Restaurante = Restaurante.new
   end
 
   def edit
   end
 
   def create
+    @restaurante = Restaurante.new(restaurante_params)
+    if @restaurante.save
+      redirect_to restaurantes_path
+    else
+      render "new"
+    end
   end
 
   def update
@@ -19,4 +28,9 @@ class RestaurantesController < ApplicationController
 
   def destroy
   end
+
+  private
+    def restaurante_params
+      params.require(:restaurante).permit(:nombre)
+    end
 end
