a1ff5ceb11bc3f908d2cf2a56dd9804bd04a8b85
Edison Zapata <zed15600@gmail.com>
2019-04-11 14:53:04 -0500

    Definiendo los campos nuleables del modelo y escribiendo las vistas básicas de los productos

--- LetsHangOut/db/migrate/20190411184744_change_models_nullable_fields.rb
+++ LetsHangOut/db/migrate/20190411184744_change_models_nullable_fields.rb
@@ -1,4 +1,20 @@
 class ChangeModelsNullableFields < ActiveRecord::Migration[5.2]
   def change
+    change_column_null(:personas, :nombre, false)
+    change_column_null(:productos, :nombre, false)
+    change_column_null(:productos, :precio, false)
+    change_column_null(:productos, :porciones, false)
+    change_column_default(:productos, :porciones, 1)
+    change_column_null(:productos, :restaurante, false)
+    change_column_null(:compras, :valor, false)
+    change_column_null(:compras, :personas, false)
+    change_column_null(:compras, :valorComun, false)
+    change_column_default(:compras, :valorComun, 0)
+    change_column_null(:restaurantes, :nombre, false)
+    change_column_null(:detalle_compras, :compra, false)
+    change_column_null(:detalle_compras, :persona, false)
+    change_column_null(:detalle_compras, :producto, false)
+    change_column_null(:pagos, :persona, false)
+    change_column_null(:pagos, :valor, false)
   end
 end
