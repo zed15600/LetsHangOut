f12f732dd400dac2c2db07826e4f704c048c4199
Edison Zapata <zed15600@gmail.com>
2019-04-11 15:16:32 -0500

    Definiendo el controlador de productos

--- LetsHangOut/app/controllers/productos_controller.rb
+++ LetsHangOut/app/controllers/productos_controller.rb
@@ -1,17 +1,26 @@
 class ProductosController < ApplicationController
+
   def index
+    @productos = Producto.all
   end
 
   def show
   end
 
   def new
+    @producto = Producto.new
   end
 
   def edit
   end
 
   def create
+    @producto = Producto.new(producto_params)
+    if @producto.save
+      redirect_to productos_path
+    else
+      render "new"
+    end
   end
 
   def update
@@ -19,4 +28,8 @@ class ProductosController < ApplicationController
 
   def destroy
   end
+
+  private
+  def producto_params
+    params.require(:producto).permit(:restaurante_id, :nombre, :precio, :porciones)
 end
